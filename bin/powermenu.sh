a7df3580c12385ae1318d2df65a2d0fe76038ab3
jvlaj <jvlaj@protonmail.com>
2021-03-17 20:58:47 +1100

    updated powermenu, removed unused scripts

--- bin/powermenu.sh
+++ bin/powermenu.sh
@@ -3,17 +3,15 @@
 rofi_command="rofi -theme ~/.config/rofi/powermenu.rasi -m -4"
 
 # Options
-shutdown="襤"
-reboot="ﰇ"
-#lock=""
-suspend="⏾"
-logout=""
-hibernate="鈴"
+shutdown=""
+reboot=""
+lock=""
+logout=""
 
 user=$(whoami)
 
 # Variable passed to rofi
-options="$shutdown\n$reboot\n$suspend\n$hibernate\n$logout"
+options="$shutdown\n$lock\n$reboot\n$logout"
 
 chosen="$(echo -e "$options" | $rofi_command -dmenu -selected-row 2)"
 
@@ -24,22 +22,16 @@ case $chosen in
     $reboot)
 		answer=$(echo -e "no\nyes" | rofi -dmenu -i -no-fixed-num-lines -p "ﰇ Reboot now?" -theme ~/.config/rofi/confirm.rasi)
         ;;
-#    $lock)
-#        ;;
-    $hibernate)
-		answer=$(echo -e "no\nyes" | rofi -dmenu -i -no-fixed-num-lines -p "鈴  Hibernate now?" -theme ~/.config/rofi/confirm.rasi)
-		;;
-    $suspend)
-		answer=$(echo -e "no\nyes" | rofi -dmenu -i -no-fixed-num-lines -p "⏾ Suspend now?" -theme ~/.config/rofi/confirm.rasi)
+     $lock)
+	answer=$(echo -e "no\nyes" | rofi -dmenu -i -no-fixed-num-lines -p " Lock now?" -theme ~/.config/rofi/confirm.rasi)
         ;;
-    $logout)
+     $logout)
 		answer=$(echo -e "no\nyes" | rofi -dmenu -i -no-fixed-num-lines -p "  Logout now?" -theme ~/.config/rofi/confirm.rasi)
         ;;
 esac
 
 [[ ${answer,,} != yes ]] && exit 1
 
-# XXX
 ### Modify to your system ###
 case $chosen in
     $shutdown)
@@ -48,17 +40,8 @@ case $chosen in
     $reboot)
         systemctl reboot
         ;;
-#    $lock)
-#	mantablockscreen
-#        ;;
-    $hibernate)
-	systemctl hibernate
-	;;
-    $suspend)
-        cmus-remote -u
-        amixer set Master mute
-	mantablockscreen &
-        systemctl suspend
+    $lock)
+	xlock
         ;;
     $logout)
         pkill -KILL -U $user
--- bin/restore-screen.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-autorandr --change
-wmctrl -s 0
-wmctrl -s 1
-wmctrl -s 2
-wmctrl -s 0
-nitrogen --restore

8d02b772aa8c7c8183440147940b7d3475a3cb1f
Jason Vlajankov <jvlaj@protonmail.com>
2021-07-17 14:22:06 +1000

    shoddy install script

---
+++ install.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+## packages
+
+pac_PKGS = "xorg-server nvidia zsh spectrwm yay keepassxc curl redshift nodejs npm git python3 python2 python-pip python2-pip zathura zathura-cb zathura-djvu zathura-pdf-poppler ranger dunst spotifyd rofi dmenu uxterm firefox qutebrowser neovim xset feh fcitx greenclip"
+aur_PKGS = "spotify-tui nerd-fonts-sf-mono nerd-fonts-iosevka rofi-greenclip"
+npm_PKGS = "neovim"
+pip_PKGS = "jedi flake8 neovim"
+
+## setup dotfiles
+
+mkdir -p $HOME/repos/dotfiles
+git clone --bare https://github.com/jvlaj/dotfiles.git $HOME/repos/dotfiles
+alias dots='/usr/bin/git --git-dir=$HOME/repos/dotfiles --work-tree=$HOME'
+dots config --local status.showUntrackedFiles no
+dots checkout -f
+
+## install theme
+
+git clone https://github.com/rayes0/sayo.git $HOME/repos/
+mkdir -p $HOME/.themes/
+cp -r $HOME/repos/sayo-dark $HOME/.themes/
+
+## install packages
+
+sudo pacman -S -y "${pac_PKGS[@]}"
+
+git clone https://aur.archlinux.org/yay.git 
+cd yay
+if [makepkg -sri --noconfirm --needed]; then
+	yay -S -y "${aur_PKGS[@]}"
+fi
+
+sudo npm install "${npm_PKGS[@]}"
+
+sudo pip install "${pip_PKGS[@]}"
+
+echo
+echo "Installation completed."
